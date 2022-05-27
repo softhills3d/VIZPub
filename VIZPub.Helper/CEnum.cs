5627936a63ba7c797d60eadf8da5d9f36dce274a
Softhills Co., Ltd <41414449+softhills3d@users.noreply.github.com>
2022-05-27 14:54:17 +0900

    Update

---
+++ VIZPub.Helper/CEnum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VIZPub.Helper
+{
+    /// <summary>
+    /// Input File Format
+    /// </summary>
+    public enum InputFileFormat
+    {
+        /// <summary>
+        /// PDMS (TRIBON, AVEVA) Review File (ASCII)
+        /// </summary>
+        REV = 0,
+
+        /// <summary>
+        /// PDMS (TRIBON, AVEVA) Review File (BINARY)
+        /// </summary>
+        RVM = 1
+    }
+
+    public enum OutputFileFormat
+    {
+
+    }
+}
---
+++ VIZPub.Helper/PublishManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VIZPub.Helper
+{
+    public class PublishManager
+    {
+    }
+}
