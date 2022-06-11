ecad68cf10e96a43099bd4ddd8c1b7820040e5ce
Softhills Co., Ltd <41414449+softhills3d@users.noreply.github.com>
2022-06-11 15:17:00 +0900

    Update

--- VIZPub.Helper/TranslateManager.cs
+++ VIZPub.Helper/TranslateManager.cs
@@ -10,5 +10,125 @@ namespace VIZPub
     /// </summary>
     public class TranslateManager
     {
+        // ================================================
+        // Attribute & Property
+        // ================================================
+        /// <summary>
+        /// VIZCoreTrans.exe Program Path
+        /// </summary>
+        internal string VIZCoreTrans_Path { get; set; }
+
+        /// <summary>
+        /// Publish Log Received
+        /// </summary>
+        public event DataEventHandler PublishLogReceived;
+
+
+        // ================================================
+        // Construction
+        // ================================================
+        /// <summary>
+        /// Construction
+        /// </summary>
+        /// <param name="vizcoretrans_path">VIZCoreTrans.exe Program Path</param>
+        public TranslateManager(string vizcoretrans_path)
+        {
+            VIZCoreTrans_Path = vizcoretrans_path;
+        }
+
+        // ================================================
+        // Event
+        // ================================================
+        private void Process_DataReceived(object sender, DataEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(e.Message);
+            Console.WriteLine(e.Message);
+
+            if (PublishLogReceived == null) return;
+
+            DataEventArgs args = new DataEventArgs();
+            args.process = e.process;
+            args.Message = e.Message;
+
+            PublishLogReceived(this, args);
+        }
+
+        private void Process_ErrorReceived(object sender, DataEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(e.Message);
+            Console.WriteLine(e.Message);
+
+            if (PublishLogReceived == null) return;
+
+            DataEventArgs args = new DataEventArgs();
+            args.process = e.process;
+            args.Message = e.Message;
+
+            PublishLogReceived(this, args);
+        }
+
+
+        // ================================================
+        // Method :: Publish
+        // ================================================
+        /// <summary>
+        /// Export VIZ
+        /// </summary>
+        /// <param name="parameter">Translate Parameter</param>
+        /// <returns>Translate Result</returns>
+        public bool ExportVIZ(TranslateParameter parameter)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Export STEP
+        /// </summary>
+        /// <param name="parameter">Translate Parameter</param>
+        /// <returns>Translate Result</returns>
+        public bool ExportStep(TranslateParameter parameter)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Export IGES
+        /// </summary>
+        /// <param name="parameter">Translate Parameter</param>
+        /// <returns>Translate Result</returns>
+        public bool ExportIges(TranslateParameter parameter)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Export 3DXML
+        /// </summary>
+        /// <param name="parameter">Translate Parameter</param>
+        /// <returns>Translate Result</returns>
+        public bool Export3DXML(TranslateParameter parameter)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Export E-BOM XML
+        /// </summary>
+        /// <param name="parameter">Translate Parameter</param>
+        /// <returns>Translate Result</returns>
+        public bool ExportEbomXml(TranslateParameter parameter)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Export Thumbnail
+        /// </summary>
+        /// <param name="parameter">Translate Parameter</param>
+        /// <returns>Translate Result</returns>
+        public bool ExportThumbnail(TranslateParameter parameter)
+        {
+            return false;
+        }
     }
 }
--- VIZPub.Test.Console/VIZCoreTransTest.cs
+++ VIZPub.Test.Console/VIZCoreTransTest.cs
@@ -16,7 +16,72 @@ namespace VIZPub.Test.Console
 
         public void Test()
         {
+            ExportVIZ();
+            ExportStep();
+            ExportIges();
+            Export3DXML();
+            ExportEbomXml();
+            ExportThumbnail();
+        }
 
+        public void ExportVIZ()
+        {
+            TranslateParameter parameter = new TranslateParameter();
+
+            // VIZCoreTrans
+            // Path : Ex) C:\SOFTHILLS\VIZPub\VIZCoreTrans.exe
+            TranslateManager translate = new TranslateManager(VIZCoreTrans_Path);
+            bool result = translate.ExportVIZ(parameter);
+        }
+
+        public void ExportStep()
+        {
+            TranslateParameter parameter = new TranslateParameter();
+
+            // VIZCoreTrans
+            // Path : Ex) C:\SOFTHILLS\VIZPub\VIZCoreTrans.exe
+            TranslateManager translate = new TranslateManager(VIZCoreTrans_Path);
+            bool result = translate.ExportStep(parameter);
+        }
+
+        public void ExportIges()
+        {
+            TranslateParameter parameter = new TranslateParameter();
+
+            // VIZCoreTrans
+            // Path : Ex) C:\SOFTHILLS\VIZPub\VIZCoreTrans.exe
+            TranslateManager translate = new TranslateManager(VIZCoreTrans_Path);
+            bool result = translate.ExportIges(parameter);
+        }
+
+        public void Export3DXML()
+        {
+            TranslateParameter parameter = new TranslateParameter();
+
+            // VIZCoreTrans
+            // Path : Ex) C:\SOFTHILLS\VIZPub\VIZCoreTrans.exe
+            TranslateManager translate = new TranslateManager(VIZCoreTrans_Path);
+            bool result = translate.Export3DXML(parameter);
+        }
+
+        public void ExportEbomXml()
+        {
+            TranslateParameter parameter = new TranslateParameter();
+
+            // VIZCoreTrans
+            // Path : Ex) C:\SOFTHILLS\VIZPub\VIZCoreTrans.exe
+            TranslateManager translate = new TranslateManager(VIZCoreTrans_Path);
+            bool result = translate.ExportEbomXml(parameter);
+        }
+
+        public void ExportThumbnail()
+        {
+            TranslateParameter parameter = new TranslateParameter();
+
+            // VIZCoreTrans
+            // Path : Ex) C:\SOFTHILLS\VIZPub\VIZCoreTrans.exe
+            TranslateManager translate = new TranslateManager(VIZCoreTrans_Path);
+            bool result = translate.ExportThumbnail(parameter);
         }
     }
 }
