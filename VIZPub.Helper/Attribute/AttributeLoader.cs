6228f6abfd980d559c6e4ed0310b89899da3abd6
Softhills Co., Ltd <41414449+softhills3d@users.noreply.github.com>
2022-07-20 22:58:32 +0900

    Key, Values

--- VIZPub.Helper/Attribute/AttributeLoader.cs
+++ VIZPub.Helper/Attribute/AttributeLoader.cs
@@ -15,6 +15,11 @@ namespace VIZPub
         // ================================================
         internal string Path { get; set; }
 
+        internal Dictionary<string, string> IKeys { get; set; }
+        internal Dictionary<string, Dictionary<string, List<AttributeItem>>> IValue { get; set; }
+
+        
+
 
         // ================================================
         // Construction
@@ -38,6 +43,9 @@ namespace VIZPub
         public bool Load(string path, out Dictionary<int, List<AttributeItem>> attribute)
         {
             attribute = new Dictionary<int, List<AttributeItem>>();
+
+            IKeys = new Dictionary<string, string>();
+            IValue = new Dictionary<string, Dictionary<string, List<AttributeItem>>>();
 
             Path = path;
 
@@ -66,6 +74,35 @@ namespace VIZPub
                         items.Add(attributeItem);
                         attribute[attributeItem.NodeID] = items;
                     }
+
+                    // Keys
+                    if (IKeys.ContainsKey(attributeItem.Key) == false)
+                        IKeys.Add(attributeItem.Key, attributeItem.Key);
+
+                    // Values
+                    if (IValue.ContainsKey(attributeItem.Key) == false)
+                    {
+                        Dictionary<string, List<AttributeItem>> iVals = new Dictionary<string, List<AttributeItem>>();
+                        iVals.Add(attributeItem.Value, new List<AttributeItem>() { attributeItem });
+
+                        IValue.Add(attributeItem.Key, iVals);
+                    }
+                    else
+                    {
+                        Dictionary<string, List<AttributeItem>> iVals = IValue[attributeItem.Key];
+
+                        if (iVals.ContainsKey(attributeItem.Value) == false)
+                        {
+                            iVals.Add(attributeItem.Value, new List<AttributeItem>() { attributeItem });
+                            IValue[attributeItem.Key] = iVals;
+                        }
+                        else
+                        {
+                            List<AttributeItem> iItem = iVals[attributeItem.Value];
+                            iItem.Add(attributeItem);
+                            iVals[attributeItem.Value] = iItem;
+                        }
+                    }
                 }
 
                 return true;
@@ -74,6 +111,44 @@ namespace VIZPub
             {
                 return false;
             }
+        }
+
+        /// <summary>
+        /// Get UDA Keys
+        /// </summary>
+        /// <returns>Keys</returns>
+        public List<string> GetKeys()
+        {
+            return IKeys.Keys.ToList();
+        }
+
+        /// <summary>
+        /// Get UDA Values
+        /// </summary>
+        /// <param name="key">UDA Key</param>
+        /// <returns>Values</returns>
+        public List<string> GetValues(string key)
+        {
+            if (IValue.ContainsKey(key) == true)
+                return IValue[key].Keys.ToList();
+            else
+                return new List<string>();
+        }
+
+        /// <summary>
+        /// Get Value Items
+        /// </summary>
+        /// <param name="key">UDA Key</param>
+        /// <param name="value">UDA Value</param>
+        /// <returns>Attribute Items</returns>
+        public List<AttributeItem> GetValueItems(string key, string value)
+        {
+            if (IValue.ContainsKey(key) == false) return new List<AttributeItem>();
+            Dictionary<string, List<AttributeItem>> values = IValue[key];
+
+            if (values.ContainsKey(value) == false) return new List<AttributeItem>();
+
+            return values[value];
         }
     }
 }
