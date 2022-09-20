ac19cd61189c0a9dc3e91cb472dddee28ba07475
Softhills Co., Ltd <gjkim@softhills.net>
2022-09-20 15:32:15 +0900

    Update

---
+++ VIZPub.Test.Console/VIZCoreTransIopTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VIZPub.Test.Console
+{
+    public class VIZCoreTransIopTest
+    {
+        public string VIZCoreTrans_Path { get; set; }
+
+        public VIZCoreTransIopTest()
+        {
+            VIZCoreTrans_Path = "D:\\SH_GitHub\\VIZPub\\VIZCoreTransIOP\\VIZCoreTrans.exe";
+        }
+
+        public void ExportVIZW()
+        {
+
+        }
+    }
+}
