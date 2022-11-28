1dad4db68f7f3b4e518027df3f3a3b771620ec6b
hblee0909 <107383022+hblee0909@users.noreply.github.com>
2022-11-28 13:56:14 +0900

    Parasolid Parameter Edit

--- VIZPub.Test.Console/VIZCoreTransTest.cs
+++ VIZPub.Test.Console/VIZCoreTransTest.cs
@@ -364,6 +364,11 @@ namespace VIZPub.Test.Console
             parameter.Add(TranslateParameters.CAD2CAD, "C:\\Temp\\VIZCoreTrans Test XML\\Parasolid");// CAD to CAD XML 경로
 
             parameter.Add(TranslateParameters.WRITE_TESSELATION, false);                // [Optional] Default(False), Tessellation 변환 여부
+            parameter.Add(TranslateParameters.SAVE_AS_BINARY, false);                   // [Optional] Default(False), 파일 Binary 저장 여부
+            parameter.Add(TranslateParameters.HIDDEN_OBJECT, false);             // [Optional] Default(False), Hidden Entity 변환 여부 (visible entity로 변환)
+            parameter.Add(TranslateParameters.STRICT_ASSEMBLY_STRUCTURE, false);        // [Optional] Default(False), Parasolid Assembly Tree를 Origin Software 방식에 가깝게 변환
+            parameter.Add(TranslateParameters.MAKE_POINT_WITH_COORDSYSTEM, false);      // [Optional] Default(False), Coordinate System을 Point로 변환 여부
+            parameter.Add(TranslateParameters.EXPLODE_MULTI_BODY, false);               // [Optional] Default(False), 다중 연결 B-rep을 Multi Body로 변환 여부
 
             TranslateManager translate = new TranslateManager(VIZCoreTrans_Path);
             bool result = translate.ExportPS(parameter);
