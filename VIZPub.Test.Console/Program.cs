b0fe5896ba7d7adf4ea49e721405c2a4f89b166a
Softhills Co., Ltd <gjkim@softhills.net>
2022-12-05 10:18:25 +0900

    Update Program.cs

--- VIZPub.Test.Console/Program.cs
+++ VIZPub.Test.Console/Program.cs
@@ -13,7 +13,7 @@ namespace VIZPub.Test.Console
             // VIZPub
             // ================================================
             VIZPubTest test_VIZPub = new VIZPubTest();
-            //test_VIZPub.Test();
+            test_VIZPub.Test();
 
             // ================================================
             // VIZCoreTrans
@@ -37,7 +37,7 @@ namespace VIZPub.Test.Console
             // VIZPub 2D
             // ================================================
             VIZPub2DTest test_VIZPub2D = new VIZPub2DTest();
-            test_VIZPub2D.Test();
+            //test_VIZPub2D.Test();
 
             // ================================================
             // VIZCoreTrans (InterOp)
